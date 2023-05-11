fcdb51a999a4e9fecc70b44ee3d571ad7e5bab94
Unashe Siwawa <anelesiwawa@gmail.com>
2023-05-11 15:31:04 +0200

    FIrst Commit
    FIrst commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const port = 3100;
+var path = require('path');
+
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname + '/public/index.html'))
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
